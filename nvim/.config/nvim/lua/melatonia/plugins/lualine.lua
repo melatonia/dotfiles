327179e87b17cd3e8ec7890f341f39eec30144f4
melatonia <110846681+melatonia@users.noreply.github.com>
2026-08-16 22:38:21 +0300

    nvim: feat: add lualine plugin

---
+++ nvim/.config/nvim/lua/melatonia/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'gruvbox-material',
+      component_separators = { left = '|', right = '|' },
+      section_separators = { left = '', right = '' },
+    },
+  },
+}
