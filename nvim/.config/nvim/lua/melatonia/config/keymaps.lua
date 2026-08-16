7f98d7eaa8a330dcb9d33b6c9c8c3094dbed9143
melatonia <110846681+melatonia@users.noreply.github.com>
2026-08-16 19:46:04 +0300

    feat: add telescope keymaps

--- nvim/.config/nvim/lua/melatonia/config/keymaps.lua
+++ nvim/.config/nvim/lua/melatonia/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- set leader key to space
 vim.g.mapleader = " "
 vim.g.maploaclleader = " "
+
+local map = vim.keymap.set
+
+-- telescope 
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "find files" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>",  { desc = "live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>",    { desc = "find bufers" })
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>",   { desc = "recent files" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>",  { desc = "help tags" })
+
