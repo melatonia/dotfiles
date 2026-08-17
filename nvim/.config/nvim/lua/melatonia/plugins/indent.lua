65a6796f922fdde2a30c4aa19c69b528b79562c0
melatonia <110846681+melatonia@users.noreply.github.com>
2026-08-17 17:10:16 +0300

    nvim: fix: remove indent lines in dashboard

--- nvim/.config/nvim/lua/melatonia/plugins/indent.lua
+++ nvim/.config/nvim/lua/melatonia/plugins/indent.lua
@@ -1,12 +1,12 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
+  event = { "BufReadPre", "BufNewFile" },
   ---@module "ibl"
   ---@type ibl.config
   opts = {
     indent = {
 			char = "│",
-      tab_char = "│",
     },
     scope = {
       enabled = true,
